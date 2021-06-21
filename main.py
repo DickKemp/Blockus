a10ce660665cee8cb9e3ed01e994dde3bf9b2861
DickKemp <dickkemp14@gmail.com>
2021-06-21 16:40:31 -0400

    add timers

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from blokus import UnequalEdgesException
 from canvas import Canvas, CompositeShape, PolygonShape, Style, Box
 from point import Point
 import math
+import time
 
-def gen_next_level(levelset, b1) -> Set[Blok]:
+def gen_next_level(levelset, b1, is_symmetric=False) -> Set[Blok]:
     unique_blocks = set()
     cntr = 0
+    if is_symmetric:
+        b1_edges = range(Blok.num_edges(b1))
+    else:
+        b1_edges = [0]
     for b0 in iter(levelset):
         for b0e in range(Blok.num_edges(b0)):
-            for b1e in range(Blok.num_edges(b1)):
+            for b1e in b1_edges:
                 try:
                     (b,bm) = Blok.align_blocks_on_edge(b0, b0e, b1, b1e)
                     newb = Blok.merge(b, bm)
@@ -23,7 +28,7 @@ def gen_next_level(levelset, b1) -> Set[Blok]:
                     flipped_newb_not_in = flipped_newb not in unique_blocks
                     if newb_not_in and flipped_newb_not_in:
                         # print(f"adding newb: {newb}: id:{newb.gen_id()}, flipped_newb_id: {flipped_newb.gen_id()}")
-                        print(f"newb: {newb}")
+                        # print(f"newb: {newb}")
                         unique_blocks.add(newb)
                 except OverlapException:
                     continue
@@ -31,7 +36,8 @@ def gen_next_level(levelset, b1) -> Set[Blok]:
                     continue
     return unique_blocks
 
-def runn(basic, num_levels, svg_file):
+def runn(name, basic, num_levels, svg_file, is_symmetric = False):
+    start_time = time.time()
     b0 = Blok(basic)
     b1 = Blok.rotate(b0, Blok.get_edge(b0,0).start_pt, math.pi/4)
     prev_level = set()
@@ -39,9 +45,8 @@ def runn(basic, num_levels, svg_file):
     all = [(1,b0)]
 
     for lev in range(num_levels-1):
-        curr_level = gen_next_level(prev_level, b1)
+        curr_level = gen_next_level(prev_level, b1, is_symmetric)
         for s in iter(curr_level):
-            #print(s)
             all.append((lev+2,s))
         prev_level = curr_level
 
@@ -61,9 +66,10 @@ def runn(basic, num_levels, svg_file):
             sh = CompositeShape(component_polygons)
         cv = Canvas.add_shape3(cv, sh, box, bigbox, margin_percent=0.3, label=f"#blocks: {str(n)}")
         i = i + 1
-    print(f"num shapes: {len(all)}")
+
     with open(svg_file, 'w') as fd:
             Canvas.render_as_svg(cv, file=fd)
+    print(f"{name}: Num shapes: {len(all)} --- Time: {(time.time() - start_time)} seconds ---")
 
 def pentagon(r):
     sum_interior_angles = (5-2)*math.pi
@@ -89,10 +95,22 @@ if __name__ == '__main__':
     ISOTRIANGLE = [Point(0.0, 0.0), Point(1.0, 1.0), Point(1.0,0.0)]
     PENTAGON = [Point(x,y) for (x,y) in pentagon(1.0)]
 
+    h = math.sin(math.pi/3.0)
+    hex = [ (-0.5,-h), (-1,0), (-0.5,h), (0.5,h), (1,0), (0.5,-h) ]
+    HEXAGON = [Point(x,y) for (x,y) in hex]
+
+    is_symmetric = True
+    
+    start_time = time.time()
     for i in range(1,8):
-        runn(SQUARE, i, f"svg/square{i}.svg")
-        runn(TRIANGLE, i, f"svg/triangle{i}.svg")
-        runn(ISOTRIANGLE, i, f"svg/iso_triangle{i}.svg")
-        runn(PENTAGON, i, f"svg/pentagon{i}.svg")
+        loop_time = time.time()
+        print(f"\tStart LOOP {i}")
+        runn("\t\tTRIANGLE", TRIANGLE, i, f"svg/triangle{i}.svg", is_symmetric)
+        runn("\t\tSQUARE", SQUARE, i, f"svg/square{i}.svg", is_symmetric)
+        runn("\t\tPENTAGON", PENTAGON, i, f"svg/pentagon{i}.svg", is_symmetric)
+        runn("\t\tHEXAGON", HEXAGON, i, f"svg/hexagon{i}.svg", is_symmetric)
+        # runn("ISOTRIANGLE", ISOTRIANGLE, i, f"svg/iso_triangle{i}.svg")
+        print(f"\tEnd LOOP {i} --- Time: {(time.time() - loop_time)} seconds ---")
         pass
+    print(f"TOTAL --- Time: {(time.time() - start_time)} seconds ---")
 
